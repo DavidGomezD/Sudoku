b15a2d8f57ad93f92ec9d3f1ac2639a1b0dec779
DavidGomezD <davidgoomez7@gmail.com>
2020-12-02 13:39:19 -0600

    Sudoku 1.1 Mejor diseño

--- codigo/script.py
+++ codigo/script.py
@@ -13,9 +13,9 @@ c = 0
 while c <= 8:
     print("""{% set mostrar_"""+str(n)+""" = aleatorio_"""+str(n)+""" %}
 {% if mostrar_"""+str(n)+""" == 0 %}
-<input class="w-full pl-7 justify-self-center bg-black text-white" type="number" name=" """+str(arreglo[c])+"""" min="1" max="9">
+<input class="w-full pl-8 justify-self-center bg-black text-white" type="number" name=" """+str(arreglo[c])+"""" min="1" max="9">
 {% else %}
-<input class="w-full pl-7 justify-self-center bg-black" type="number" name=" """+str(arreglo[c])+""" " value="{{  """+str(arreglo[c])+"""  }}" readonly="readonly">
+<input class="w-full pl-8 justify-self-center bg-black" type="number" name=" """+str(arreglo[c])+""" " value="{{  """+str(arreglo[c])+"""  }}" readonly="readonly">
 {% endif %}""")
     c += 1
     n += 1
